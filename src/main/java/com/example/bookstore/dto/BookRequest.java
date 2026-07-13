9032fc954a248d962d3d0fb630dd7c1eeafd1099
“OmarNabil005” <omarnabil219@gmail.com>
2026-07-14 00:13:35 +0300

    feat: added book entity and dto

---
+++ src/main/java/com/example/bookstore/dto/BookRequest.java
@@ -0,0 +1,63 @@
+package com.example.bookstore.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+public class BookRequest {
+
+    @NotBlank(message = "Title is required")
+    private String title;
+
+    @NotBlank(message = "Author is required")
+    private String author;
+
+    @NotBlank(message = "ISBN is required")
+    private String isbn;
+
+    @NotNull(message = "Price is required")
+    @Positive(message = "Price must be positive")
+    private Double price;
+
+    public BookRequest() {
+    }
+
+    public BookRequest(String title, String author, String isbn, Double price) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
